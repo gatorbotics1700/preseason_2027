59fc92cbb1a11aeb64514f72c734818bccfe7ce1
Violet Chaffee <26vchaffee@castilleja.org>
2025-05-20 16:51:06 -0700

    set constants in advantage kit

---
+++ src/main/java/frc/com/swervedrivespecialties/swervelib/DriveController.java
@@ -0,0 +1,13 @@
+package frc.com.swervedrivespecialties.swervelib;
+
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+
+public interface DriveController {
+    MotorController getDriveMotor();
+
+    void setReferenceVoltage(double voltage);
+
+    double getStateVelocity();
+
+    double getStateDistance();
+}
