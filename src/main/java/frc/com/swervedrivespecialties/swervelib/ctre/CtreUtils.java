5d12ab003b032daac4ece29daac17f961be4f1d0
Gatorbotics <gatorbotics1700@gmail.com>
2025-05-21 19:02:59 -0700

    set constants for advantagekit

--- src/main/java/frc/com/swervedrivespecialties/swervelib/ctre/CtreUtils.java
+++ src/main/java/frc/com/swervedrivespecialties/swervelib/ctre/CtreUtils.java
@@ -5,11 +5,11 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotBase;
 
 public final class CtreUtils {
-    private CtreUtils() {}
+  private CtreUtils() {}
 
-    public static void checkCtreError(StatusCode code, String message) {
-        if (RobotBase.isReal() && code != StatusCode.OK) {
-            DriverStation.reportError(String.format("%s: %s", message, code.toString()), false);
-        }
+  public static void checkCtreError(StatusCode code, String message) {
+    if (RobotBase.isReal() && code != StatusCode.OK) {
+      DriverStation.reportError(String.format("%s: %s", message, code.toString()), false);
     }
+  }
 }
