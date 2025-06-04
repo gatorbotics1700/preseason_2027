bf23bfc573b67fa2384986891ac8d61fd3047ccc
Gatorbotics <gatorbotics1700@gmail.com>
2025-06-04 14:20:33 -0700

    got advantagekit build files to main and used that to create advantagekitbase branch

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,11 +1,13 @@
 package frc.robot;
 
+import org.littletonrobotics.junction.LoggedRobot;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
     private Command m_autonomousCommand;
     private RobotContainer container;
 
@@ -25,7 +27,7 @@ public class Robot extends TimedRobot {
 
     @Override
     public void autonomousInit() {
-        m_autonomousCommand = container.getAutonomousCommand();
+        // m_autonomousCommand = container.getAutonomousCommand();
 
         if (m_autonomousCommand != null) {
             m_autonomousCommand.schedule();
@@ -34,7 +36,7 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopInit() {
-        container.setDefaultTeleopCommand();
+        // container.setDefaultTeleopCommand();
 
         // This makes sure that the autonomous stops running when teleop starts
         if (m_autonomousCommand != null) {
