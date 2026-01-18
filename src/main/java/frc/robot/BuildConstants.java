41b035284299efff141215746d12f1c11c1c314e
Violet Chaffee <26vchaffee@castilleja.org>
2026-01-17 16:46:48 -0800

    fixed build problems

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,14 +5,14 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "1700_Robot_2026";
+  public static final String MAVEN_NAME = "1700_Robot_2026-Imported";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 4;
-  public static final String GIT_SHA = "79c3c2903bb7ce5d08820902d3bbd4f60d083a05";
-  public static final String GIT_DATE = "2026-01-17 13:44:52 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-17 15:02:19 EST";
-  public static final long BUILD_UNIX_TIME = 1768680139953L;
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "483eed2b660cc9dacd059183957bf1c76b1009af";
+  public static final String GIT_DATE = "2026-01-17 15:51:04 EST";
+  public static final String GIT_BRANCH = "photonvision";
+  public static final String BUILD_DATE = "2026-01-17 19:40:47 EST";
+  public static final long BUILD_UNIX_TIME = 1768696847366L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
