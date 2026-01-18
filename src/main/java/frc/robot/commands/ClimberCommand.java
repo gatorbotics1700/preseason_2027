d40c5a2d7f9e82acbe5094dd821251383baa0deb
gatorbotics <gatorbotics1700@gmail.com>
2026-01-17 16:38:11 -0800

    idk

--- src/main/java/frc/robot/commands/ClimberCommand.java
+++ src/main/java/frc/robot/commands/ClimberCommand.java
@@ -24,22 +24,43 @@ public class ClimberCommand extends Command {
         if (level == 0){
             double groundPosition = climberSubsystem.getCurrentTicksOuter() - climberSubsystem.InchesToTicks(Constants.LOW_RUNG_ARM_LENGTH);
             climberSubsystem.extendOuterArm(groundPosition);
+            System.out.println("RETURNING TO GROUND");
         } else if(level == 1){
             double LowPosition = climberSubsystem.getCurrentTicksOuter() + climberSubsystem.InchesToTicks(Constants.LOW_RUNG_ARM_LENGTH);
             climberSubsystem.extendOuterArm(LowPosition);
             double lowRetracted = climberSubsystem.getCurrentTicksOuter() + climberSubsystem.InchesToTicks(Constants.LOW_RUNG_ARM_RETRACTED);
             climberSubsystem.extendOuterArm(lowRetracted);
+            System.out.println("AT L1");
         } else if (level == 2){
 
         } else if (level == 3){
             double lowPosition = climberSubsystem.getCurrentTicksOuter() + climberSubsystem.InchesToTicks(Constants.LOW_RUNG_ARM_LENGTH);
             climberSubsystem.extendOuterArm(lowPosition);
+
             double lowRetracted = climberSubsystem.getCurrentTicksOuter() + climberSubsystem.InchesToTicks(Constants.LOW_RUNG_ARM_RETRACTED);
             climberSubsystem.extendOuterArm(lowRetracted);
+
             double midPosition = climberSubsystem.getCurrentTicksOuter() + climberSubsystem.InchesToTicks(Constants.MID_RUNG_ARM_LENGTH);
             climberSubsystem.extendInnerArm(midPosition);
-            midPosition = climberSubsystem.getCurrentTicksInner() + climberSubsystem.InchesToTicks(Constants.MID_RUNG_ARM_LENGTH);  //check if arms will have to extend different lengths here
+
+            midPosition = climberSubsystem.getCurrentTicksInner() + climberSubsystem.InchesToTicks(Constants.MID_RUNG_ARM_LENGTH);  //check if arms will have to extend and retract different lengths
             climberSubsystem.extendOuterArm(midPosition);
+
+            double midRetracted = climberSubsystem.getCurrentTicksOuter() + climberSubsystem.InchesToTicks(Constants.MID_RUNG_ARM_RETRACTED);
+            climberSubsystem.extendOuterArm(midRetracted);
+
+            midRetracted = climberSubsystem.getCurrentTicksInner() + climberSubsystem.InchesToTicks(Constants.MID_RUNG_ARM_RETRACTED);
+            climberSubsystem.extendInnerArm(midRetracted);
+
+            double highPosition = climberSubsystem.getCurrentTicksInner() + climberSubsystem.InchesToTicks(Constants.HIGH_RUNG_ARM_LENGTH);
+            climberSubsystem.extendInnerArm(highPosition);
+
+            highPosition = climberSubsystem.getCurrentTicksOuter() + climberSubsystem.InchesToTicks(Constants.HIGH_RUNG_ARM_LENGTH);
+            climberSubsystem.extendOuterArm(highPosition);
+
+            double highRetracted = climberSubsystem.getCurrentTicksOuter() + climberSubsystem.InchesToTicks(Constants.HIGH_RUNG_ARM_RETRACTED);
+            climberSubsystem.extendOuterArm(highRetracted);
+            System.out.println("AT L3!!!");
         } else {
             System.out.println("*** NOT VALID LEVEL ***");
         }
