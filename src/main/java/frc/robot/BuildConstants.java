33e760def9fc63e3222a88cd917cef1b3d8ac8cb
Violet Chaffee <26vchaffee@castilleja.org>
2026-01-23 17:12:08 -0800

    added autoformatter to build.gradle

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,19 +1,17 @@
 package frc.robot;
 
-/**
- * Automatically generated file containing build version information.
- */
+/** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "1700_Robot_2026-1";
+  public static final String MAVEN_NAME = "1700_Robot_2026-Imported";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 5;
-  public static final String GIT_SHA = "4d67446e3cba2869e9c7628d28ea1bc71f460ffe";
-  public static final String GIT_DATE = "2026-01-17 15:27:10 EST";
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "b98d002b8bf630a0fd5fe1ad395accb0203d9b4c";
+  public static final String GIT_DATE = "2026-01-22 15:35:47 PST";
   public static final String GIT_BRANCH = "autopaths_annekel";
-  public static final String BUILD_DATE = "2026-01-21 18:39:17 EST";
-  public static final long BUILD_UNIX_TIME = 1769038757684L;
+  public static final String BUILD_DATE = "2026-01-23 17:11:27 PST";
+  public static final long BUILD_UNIX_TIME = 1769217087286L;
   public static final int DIRTY = 1;
 
-  private BuildConstants(){}
+  private BuildConstants() {}
 }
