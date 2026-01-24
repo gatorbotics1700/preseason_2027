16c185546bebe85d11d9bf600a27c331f7da295f
Violet Chaffee <26vchaffee@castilleja.org>
2026-01-23 17:27:56 -0800

    installed elasticLib, wrote a line to see if the tab switches to teleop when teleop begins

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "1700_Robot_2026-Imported";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 10;
-  public static final String GIT_SHA = "c96a178715032e7f64c0dd33891e7d4ef75d28d0";
-  public static final String GIT_DATE = "2026-01-22 15:41:06 PST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-23 16:58:00 PST";
-  public static final long BUILD_UNIX_TIME = 1769216280981L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 11;
+  public static final String GIT_SHA = "b39377cb280f210f596a47c9917ba850ea2f4b1b";
+  public static final String GIT_DATE = "2026-01-23 16:58:25 PST";
+  public static final String GIT_BRANCH = "elastic";
+  public static final String BUILD_DATE = "2026-01-23 17:24:59 PST";
+  public static final long BUILD_UNIX_TIME = 1769217899360L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
