d1d125669eeabb999332899d961f19f532ceed6f
Anne Kelley <annekelley@Annes-MacBook-Air.local>
2026-01-30 16:13:50 -0800

    button binded

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,14 +3,14 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "1700_Robot_2026-Imported";
+  public static final String MAVEN_NAME = "1700_Robot_2026-1";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 13;
-  public static final String GIT_SHA = "acf38bac3a694b37f62133c8840416974c7f4c5d";
-  public static final String GIT_DATE = "2026-01-27 19:22:43 PST";
+  public static final int GIT_REVISION = 16;
+  public static final String GIT_SHA = "f29dd8a768844f82aa1272387daf26bbd3ce27af";
+  public static final String GIT_DATE = "2026-01-30 15:42:37 PST";
   public static final String GIT_BRANCH = "autopaths_annekel";
-  public static final String BUILD_DATE = "2026-01-27 19:43:50 PST";
-  public static final long BUILD_UNIX_TIME = 1769571830777L;
+  public static final String BUILD_DATE = "2026-01-30 16:07:24 PST";
+  public static final long BUILD_UNIX_TIME = 1769818044245L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/commands/DriveOverBumpCommand.java
+++ src/main/java/frc/robot/commands/DriveOverBumpCommand.java
@@ -11,29 +11,22 @@ import org.json.simple.parser.ParseException;
 public class DriveOverBumpCommand {
 
   // y value to split top vs bottom half of field
-  private static final double BUMP_Y = 4.034663; //METERSSSSSS
+  private static final double BUMP_Y = 4.034663; // METERSSSSSS
   // x value to split left vs right half of field
-  private static final double BUMP_X = 8.270494; //meters
+  private static final double BUMP_X = 8.270494; // meters
 
   public static Command driveOverBump(Drive drive) throws IOException, ParseException {
     return new ConditionalCommand(
         // y < BUMP_Y (bottom half) — go to top me thinks
         new ConditionalCommand(
-           
-            AutoBuilder.followPath(PathPlannerPath.fromPathFile("Over Left Bump Bottom to Top")),
-            AutoBuilder.followPath(PathPlannerPath.fromPathFile("Over Right Bump Bottom to Top")),
-         
+            AutoBuilder.followPath(PathPlannerPath.fromPathFile("B BR A to N")),
+            AutoBuilder.followPath(PathPlannerPath.fromPathFile("B BL A to N")),
             () -> drive.getPose().getX() < BUMP_X),
         // Y >= BUMP_Y (top half) — go to bottom me thinks
         new ConditionalCommand(
-
-            AutoBuilder.followPath(PathPlannerPath.fromPathFile("Over Left Bump Top to Bottom")),
-            AutoBuilder.followPath(PathPlannerPath.fromPathFile("Over Right Bump Top to Bottom")),
-        
+            AutoBuilder.followPath(PathPlannerPath.fromPathFile("R BL A to N")),
+            AutoBuilder.followPath(PathPlannerPath.fromPathFile("R BL to A N")),
             () -> drive.getPose().getX() < BUMP_X),
         () -> drive.getPose().getY() < BUMP_Y);
-
-
-        
   }
 }
