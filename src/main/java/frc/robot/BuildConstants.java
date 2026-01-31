f4eb311718264aa74203aff39b0e6cd661e781f4
gatorbotics <gatorbotics1700@gmail.com>
2026-01-31 13:23:39 -0800

    IT WORKSSSSS (we don't know why this logic works sorry team

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "1700_Robot_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 19;
-  public static final String GIT_SHA = "77af41269e978f7483ced05482ac3e9a94e2364f";
-  public static final String GIT_DATE = "2026-01-31 09:46:51 PST";
+  public static final int GIT_REVISION = 21;
+  public static final String GIT_SHA = "0a28570878d4449d08e3fc2085f102d826176e9b";
+  public static final String GIT_DATE = "2026-01-31 13:01:32 PST";
   public static final String GIT_BRANCH = "test";
-  public static final String BUILD_DATE = "2026-01-31 11:52:32 PST";
-  public static final long BUILD_UNIX_TIME = 1769889152881L;
+  public static final String BUILD_DATE = "2026-01-31 13:22:08 PST";
+  public static final long BUILD_UNIX_TIME = 1769894528038L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/commands/DriveOverBumpCommand.java
+++ src/main/java/frc/robot/commands/DriveOverBumpCommand.java
@@ -7,7 +7,6 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.drive.Drive;
 import java.io.IOException;
 import org.json.simple.parser.ParseException;
@@ -19,7 +18,8 @@ public class DriveOverBumpCommand {
   private static final double RED_BUMP_X = 11.915;
 
   public static Command driveOverBump(Drive drive) throws IOException, ParseException {
-    PathConstraints constraints = new PathConstraints(8, 8, Units.degreesToRadians(700), Units.degreesToRadians(1000));
+    PathConstraints constraints =
+        new PathConstraints(8, 8, Units.degreesToRadians(700), Units.degreesToRadians(1000));
 
     Pose2d pose = drive.getPose();
     System.out.println("*********************DRIVE OVER BUMP COMMAND*******************");
@@ -27,30 +27,30 @@ public class DriveOverBumpCommand {
     if (pose.getX() <= CENTER.getX()) { // BLUE
       if (pose.getY() < CENTER.getY() && pose.getX() < BLUE_BUMP_X) {
         return AutoBuilder.pathfindThenFollowPath(
-            PathPlannerPath.fromPathFile("B BR A to N"), constraints);
+            PathPlannerPath.fromPathFile("B BR N to A"), constraints);
       } else if (pose.getY() > CENTER.getY() && pose.getX() < BLUE_BUMP_X) {
         return AutoBuilder.pathfindThenFollowPath(
-            PathPlannerPath.fromPathFile("B BL A to N"), constraints);
+            PathPlannerPath.fromPathFile("B BL N to A"), constraints);
       } else if ((pose.getY() > CENTER.getY()) && (pose.getX() > BLUE_BUMP_X)) {
         return AutoBuilder.pathfindThenFollowPath(
-            PathPlannerPath.fromPathFile("B BL N to A"), constraints);
+            PathPlannerPath.fromPathFile("B BL A to N"), constraints);
       } else {
         return AutoBuilder.pathfindThenFollowPath(
-            PathPlannerPath.fromPathFile("B BR N to A"), constraints);
+            PathPlannerPath.fromPathFile("B BR A to N"), constraints);
       }
     } else { // RED
       if (pose.getY() < CENTER.getY() && pose.getX() < RED_BUMP_X) {
         return AutoBuilder.pathfindThenFollowPath(
-            PathPlannerPath.fromPathFile("R BL N to A"), constraints);
+            PathPlannerPath.fromPathFile("R BL A to N"), constraints);
       } else if (pose.getY() > CENTER.getY() && pose.getX() < RED_BUMP_X) {
         return AutoBuilder.pathfindThenFollowPath(
-            PathPlannerPath.fromPathFile("R BR N to A"), constraints);
+            PathPlannerPath.fromPathFile("R BR A to N"), constraints);
       } else if ((pose.getY() > CENTER.getY()) && (pose.getX() > RED_BUMP_X)) {
         return AutoBuilder.pathfindThenFollowPath(
-            PathPlannerPath.fromPathFile("R BR A to N"), constraints);
+            PathPlannerPath.fromPathFile("R BR N to A"), constraints);
       } else {
         return AutoBuilder.pathfindThenFollowPath(
-            PathPlannerPath.fromPathFile("R BL A to N"), constraints);
+            PathPlannerPath.fromPathFile("R BL N to A"), constraints);
       }
     }
   }
