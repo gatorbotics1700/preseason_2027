639588cf16ad3f539169e668906f8a73b772d6d7
Violet Chaffee <26vchaffee@castilleja.org>
2026-02-01 17:16:47 -0800

    Squashed commit of the following:
    commit e9266f1a406dacabb30d812e7ad73fcea5280d54
    Author: gatorbotics <gatorbotics1700@gmail.com>
    Date:   Tue Jan 27 15:25:36 2026 -0800
    
        drivers practice changes

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,7 +3,7 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "1700_Robot_2026-Imported";
+  public static final String MAVEN_NAME = "1700_Robot_2026";
   public static final String VERSION = "unspecified";
   public static final int GIT_REVISION = 40;
   public static final String GIT_SHA = "c642319e18f1c743b8f55441b91430b423dbea46";
