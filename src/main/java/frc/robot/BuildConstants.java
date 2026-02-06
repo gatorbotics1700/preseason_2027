eeb010165d79ddcb16b3a1270cc9e9d2f0819f38
Violet Chaffee <26vchaffee@castilleja.org>
2026-02-06 12:21:14 -0800

    fixed any naming inconsistencies in path names

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "1700_Robot_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 104;
-  public static final String GIT_SHA = "47834f86fbe659d30de171e2d3c4ba078120918b";
-  public static final String GIT_DATE = "2026-02-06 09:29:07 PST";
+  public static final int GIT_REVISION = 106;
+  public static final String GIT_SHA = "aaad3f2414bd882ebed3648acc5b15868046b4bf";
+  public static final String GIT_DATE = "2026-02-06 12:06:49 PST";
   public static final String GIT_BRANCH = "AUTO_PATHS_WOOOOHOOOOO";
-  public static final String BUILD_DATE = "2026-02-06 11:19:06 PST";
-  public static final long BUILD_UNIX_TIME = 1770405546183L;
+  public static final String BUILD_DATE = "2026-02-06 12:12:54 PST";
+  public static final long BUILD_UNIX_TIME = 1770408774028L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
