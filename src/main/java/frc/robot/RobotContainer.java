e50a4e546ae7296eb8e954e48c6590b955b11196
gatorbotics <gatorbotics1700@gmail.com>
2026-03-20 13:46:28 -0700

    odometry works

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -114,9 +114,9 @@ public class RobotContainer {
             new Vision(
                 drive,
                 new VisionIOPhotonVision(
-                    VisionConstants.CAMERA_0_NAME, VisionConstants.ROBOT_TO_CAMERA_0));
-        // new VisionIOPhotonVision(
-        //     VisionConstants.CAMERA_1_NAME, VisionConstants.ROBOT_TO_CAMERA_1));
+                    VisionConstants.CAMERA_0_NAME, VisionConstants.ROBOT_TO_CAMERA_0),
+                new VisionIOPhotonVision(
+                    VisionConstants.CAMERA_1_NAME, VisionConstants.ROBOT_TO_CAMERA_1));
         break;
 
       case SIM:
@@ -135,11 +135,11 @@ public class RobotContainer {
                 new VisionIOPhotonVisionSim(
                     VisionConstants.CAMERA_0_NAME,
                     VisionConstants.ROBOT_TO_CAMERA_0,
+                    drive::getPose),
+                new VisionIOPhotonVisionSim(
+                    VisionConstants.CAMERA_1_NAME,
+                    VisionConstants.ROBOT_TO_CAMERA_1,
                     drive::getPose));
-        // new VisionIOPhotonVisionSim(
-        //     VisionConstants.CAMERA_1_NAME,
-        //     VisionConstants.ROBOT_TO_CAMERA_1,
-        //     drive::getPose));
         DriverStation.silenceJoystickConnectionWarning(true);
         break;
 
