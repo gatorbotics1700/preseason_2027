feb65c582268c55d38f22025506e74c5b81f7272
vivienguo <27vguo@castilleja.org>
2026-08-29 19:20:25 -0700

    added import so code built

--- src/main/java/frc/robot/commands/mech/ElevatorCommand.java
+++ src/main/java/frc/robot/commands/mech/ElevatorCommand.java
@@ -1,29 +1,30 @@
 package frc.robot.commands.mech;
+
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.mech.ElevatorSubsystem;
 
-public class ElevatorCommand extends Command{
-    
-    private int rotations;
-    private final ElevatorSubsystem elevatorSubsystem;
+public class ElevatorCommand extends Command {
 
-    public ElevatorCommand(ElevatorSubsystem elevatorSubsystem, int rotations){
-        this.elevatorSubsystem = elevatorSubsystem;
-        addRequirements(elevatorSubsystem);
-        this.rotations = rotations;
-    }
+  private int rotations;
+  private final ElevatorSubsystem elevatorSubsystem;
 
-    public void initialize() {
-        elevatorSubsystem.setTargetPosition(rotations);
-    }
+  public ElevatorCommand(ElevatorSubsystem elevatorSubsystem, int rotations) {
+    this.elevatorSubsystem = elevatorSubsystem;
+    addRequirements(elevatorSubsystem);
+    this.rotations = rotations;
+  }
 
-    public void execute() {}
+  public void initialize() {
+    elevatorSubsystem.setTargetPosition(rotations);
+  }
 
-    public boolean isFinished() {
-        return elevatorSubsystem.hasReachedTarget(rotations);
-    }
+  public void execute() {}
 
-    public void end(boolean interrupted) {
-        elevatorSubsystem.stop();
-    }
+  public boolean isFinished() {
+    return elevatorSubsystem.hasReachedTarget(rotations);
+  }
 
+  public void end(boolean interrupted) {
+    elevatorSubsystem.stop();
+  }
 }
--- src/main/java/frc/robot/subsystems/mech/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/mech/ElevatorSubsystem.java
@@ -1,120 +1,125 @@
 package frc.robot.subsystems.mech;
 
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
-
-import org.littletonrobotics.junction.networktables.LoggedNetworkNumber;
-
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-
-import com.ctre.phoenix6.configs.MotorOutputConfigs;
-import com.ctre.phoenix6.configs.Slot0Configs;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.networktables.LoggedNetworkNumber;
 
 public class ElevatorSubsystem extends SubsystemBase {
 
-    private final TalonFX motor = new TalonFX(1, "rio"); //CAN ID currently placeholder value
-    private TalonFXConfiguration talonFXConfigs; // allows us to store tuning constants
-    private final MotionMagicVoltage m_request = new MotionMagicVoltage(0); 
+  private final TalonFX motor = new TalonFX(1, "rio"); // CAN ID currently placeholder value
+  private TalonFXConfiguration talonFXConfigs; // allows us to store tuning constants
+  private final MotionMagicVoltage m_request = new MotionMagicVoltage(0);
 
-    public static final LoggedNetworkNumber elevatorKp = new LoggedNetworkNumber("/Tuning/Elevator/kP", 30);
-    public static final LoggedNetworkNumber elevatorKi = new LoggedNetworkNumber("/Tuning/Elevator/kI", 0.0);
-    public static final LoggedNetworkNumber elevatorKd = new LoggedNetworkNumber("/Tuning/Elevator/kD", 0.1);
+  public static final LoggedNetworkNumber elevatorKp =
+      new LoggedNetworkNumber("/Tuning/Elevator/kP", 30);
+  public static final LoggedNetworkNumber elevatorKi =
+      new LoggedNetworkNumber("/Tuning/Elevator/kI", 0.0);
+  public static final LoggedNetworkNumber elevatorKd =
+      new LoggedNetworkNumber("/Tuning/Elevator/kD", 0.1);
 
-    public static final LoggedNetworkNumber elevatorKg = new LoggedNetworkNumber("/Tuning/Elevator/kG", 0.2);
-    public static final LoggedNetworkNumber elevatorKs = new LoggedNetworkNumber("/Tuning/Elevator/kS", 0.01);
-    public static final LoggedNetworkNumber elevatorKv = new LoggedNetworkNumber("/Tuning/Elevator/kV", 0.16);
-    public static final LoggedNetworkNumber elevatorKa = new LoggedNetworkNumber("/Tuning/Elevator/kA", 0.01);
+  public static final LoggedNetworkNumber elevatorKg =
+      new LoggedNetworkNumber("/Tuning/Elevator/kG", 0.2);
+  public static final LoggedNetworkNumber elevatorKs =
+      new LoggedNetworkNumber("/Tuning/Elevator/kS", 0.01);
+  public static final LoggedNetworkNumber elevatorKv =
+      new LoggedNetworkNumber("/Tuning/Elevator/kV", 0.16);
+  public static final LoggedNetworkNumber elevatorKa =
+      new LoggedNetworkNumber("/Tuning/Elevator/kA", 0.01);
 
+  public ElevatorSubsystem() {
 
-    public ElevatorSubsystem(){
+    talonFXConfigs = new TalonFXConfiguration();
+    MotionMagicConfigs motionMagicConfigs = talonFXConfigs.MotionMagic;
 
-        talonFXConfigs = new TalonFXConfiguration();
-        MotionMagicConfigs motionMagicConfigs = talonFXConfigs.MotionMagic;
-
-        talonFXConfigs.withMotorOutput(
+    talonFXConfigs.withMotorOutput(
         new MotorOutputConfigs()
             .withInverted(InvertedValue.CounterClockwise_Positive)
             .withNeutralMode(NeutralModeValue.Brake));
 
-        motionMagicConfigs.MotionMagicCruiseVelocity = 100.0; // target maximum velocity (rotations/sec)
-        motionMagicConfigs.MotionMagicAcceleration = 40.0; // target maximum acceleration (rotations/sec^2)
-        motionMagicConfigs.MotionMagicJerk = 400.0; // target maximum acceleration (rotations/sec^3), or how fast it can change acceleration
+    motionMagicConfigs.MotionMagicCruiseVelocity = 100.0; // target maximum velocity (rotations/sec)
+    motionMagicConfigs.MotionMagicAcceleration =
+        40.0; // target maximum acceleration (rotations/sec^2)
+    motionMagicConfigs.MotionMagicJerk =
+        400.0; // target maximum acceleration (rotations/sec^3), or how fast it can change
+    // acceleration
 
-        Slot0Configs slot0Configs = talonFXConfigs.Slot0;
+    Slot0Configs slot0Configs = talonFXConfigs.Slot0;
 
-        // Initial PID gains come from tunable LoggedNetworkNumbers
-        slot0Configs.kP = elevatorKp.get(); // Output per unit of error in position (output/rotation)
-        slot0Configs.kI = elevatorKi.get(); // Output per unit of integrated error in position (output/(rotation*s))
-        slot0Configs.kD = elevatorKd.get(); // Output per unit of error in velocity (output/rps)
+    // Initial PID gains come from tunable LoggedNetworkNumbers
+    slot0Configs.kP = elevatorKp.get(); // Output per unit of error in position (output/rotation)
+    slot0Configs.kI =
+        elevatorKi.get(); // Output per unit of integrated error in position (output/(rotation*s))
+    slot0Configs.kD = elevatorKd.get(); // Output per unit of error in velocity (output/rps)
 
-        slot0Configs.kG = elevatorKg.get(); // Add ____V output to overcome gravity
-        slot0Configs.kS = elevatorKs.get(); // Add _____ V output to overcome static friction
+    slot0Configs.kG = elevatorKg.get(); // Add ____V output to overcome gravity
+    slot0Configs.kS = elevatorKs.get(); // Add _____ V output to overcome static friction
 
-        slot0Configs.kV = elevatorKv.get(); // Output per unit of target velocity (output/rps)
-        slot0Configs.kA = elevatorKa.get(); // Output per unit of target (output/(rps/s))
+    slot0Configs.kV = elevatorKv.get(); // Output per unit of target velocity (output/rps)
+    slot0Configs.kA = elevatorKa.get(); // Output per unit of target (output/(rps/s))
 
-        motor.getConfigurator().apply(talonFXConfigs);
+    motor.getConfigurator().apply(talonFXConfigs);
+  }
 
+  public void setTargetPosition(double targetRotations) {
+    motor.setControl(m_request.withPosition(targetRotations));
+  }
+
+  public double getCurrentPosition() {
+    return motor.getPosition().refresh().getValueAsDouble();
+  }
+
+  public boolean hasReachedTarget(double targetRotations) {
+    return Math.abs(getCurrentPosition() - targetRotations)
+        <= 0.01; // allowed error currently placeholder value
+  }
+
+  public double getVelocity() {
+    return motor.getVelocity().refresh().getValueAsDouble();
+  }
+
+  public void stop() {
+    motor.stopMotor();
+  }
+
+  public void periodic() {
+
+    Slot0Configs slot0Configs = talonFXConfigs.Slot0;
+
+    // new values updated in logged network
+
+    double newKp = elevatorKp.get();
+    double newKi = elevatorKi.get();
+    double newKd = elevatorKd.get();
+
+    double newKg = elevatorKg.get();
+    double newKs = elevatorKs.get();
+    double newKv = elevatorKv.get();
+    double newKa = elevatorKa.get();
+
+    if (newKp != slot0Configs.kP
+        || newKi != slot0Configs.kI
+        || newKd != slot0Configs.kD
+        || newKg != slot0Configs.kG
+        || newKs != slot0Configs.kS
+        || newKv != slot0Configs.kV
+        || newKa != slot0Configs.kA) {
+
+      slot0Configs.kP = newKp;
+      slot0Configs.kI = newKi;
+      slot0Configs.kD = newKd;
+      slot0Configs.kG = newKg;
+      slot0Configs.kS = newKs;
+      slot0Configs.kV = newKv;
+      slot0Configs.kA = newKa;
+
+      motor.getConfigurator().apply(slot0Configs);
     }
-
-    public void setTargetPosition(double targetRotations) {
-        motor.setControl(m_request.withPosition(targetRotations));
-    }
-
-    public double getCurrentPosition(){
-        return motor.getPosition().refresh().getValueAsDouble();
-    }
-
-    public boolean hasReachedTarget(double targetRotations) {
-        return Math.abs(getCurrentPosition() - targetRotations) <= 0.01; // allowed error currently placeholder value
-    }
-
-    public double getVelocity(){
-        return motor.getVelocity().refresh().getValueAsDouble();
-    }
-
-    public void stop(){
-        motor.stopMotor();
-    }
-
-    public void periodic(){ 
-
-        Slot0Configs slot0Configs = talonFXConfigs.Slot0;
-
-        //new values updated in logged network
-
-        double newKp = elevatorKp.get();
-        double newKi = elevatorKi.get();
-        double newKd = elevatorKd.get();
-
-        double newKg = elevatorKg.get();
-        double newKs = elevatorKs.get();
-        double newKv = elevatorKv.get();
-        double newKa = elevatorKa.get();
-
-        if (newKp != slot0Configs.kP
-            || newKi != slot0Configs.kI
-            || newKd != slot0Configs.kD
-            || newKg != slot0Configs.kG
-            || newKs != slot0Configs.kS
-            || newKv != slot0Configs.kV
-            || newKa != slot0Configs.kA) {
-
-                slot0Configs.kP = newKp;
-                slot0Configs.kI = newKi;
-                slot0Configs.kD = newKd;
-                slot0Configs.kG = newKg;
-                slot0Configs.kS = newKs;
-                slot0Configs.kV = newKv;
-                slot0Configs.kA = newKa;
-
-                motor.getConfigurator().apply(slot0Configs);
-            }
-
-        }
+  }
 }
